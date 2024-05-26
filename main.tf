2bad2373393c336e6346a41363cb10702bb7132d
Kshitij Prabhu <114593715+prabhu-ks@users.noreply.github.com>
2024-05-26 02:04:09 -0400

    Github Actions Commit-2

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-# Associate the Route Table with the public subnet
+# Associate the Route Table
 resource "aws_route_table_association" "route_table_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.route_table.id
